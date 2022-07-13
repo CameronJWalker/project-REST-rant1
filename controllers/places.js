78181d630a31f22a3de667650c228b2e0eb50490
CameronJWalker <cameron.jr.walker@icloud.com>
2022-07-12 23:45:35 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -4,6 +4,20 @@ const places = require('../models/places.js')
 app.get('/new', (req, res) => {
   res.render('places/new')
 })
+app.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
 app.get('/', (req, res) => {
       res.render('places/index', { places })
     })
