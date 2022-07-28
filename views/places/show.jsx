df6c348e38c1a5125cfc1854b752e01ef7b4b1ce
CameronJWalker <cameron.jr.walker@icloud.com>
2022-07-27 19:28:40 -0700

    added star ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,11 +7,31 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+          return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+          stars += 'â­ï¸'
+        }
+        rating = (
+          <h3>
+            {stars} stars
+          </h3>
+        )
+      
+        
         comments = data.place.comments.map(c => {
           return (
             <div className="border">
-              <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+              <h2 className="rant">{c.rant ? 'Rant! ☺' : 'Rave! ☺'}</h2>
               <h4>{c.content}</h4>
               <h3>
                 <stong>- {c.author}</stong>
@@ -34,6 +54,7 @@ function show (data) {
                         <div className="col-md" id="PlaceInfo">
                             <h1>{data.place.name}</h1>
                             <h2>Ratings</h2>
+                            {rating}
                             <p>Not Rated</p>
                             <h2>Description</h2>
                             <h3>
@@ -64,9 +85,7 @@ function show (data) {
             </main>
         </Def>
     )
-   
-}
-
+    }
 
 module.exports = show
 
